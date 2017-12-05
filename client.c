477c8844e351a5fb97a5867e5f53a4eecac1a614
Boris E. Onufriyev <beo19@atlas.cs.rutgers.edu>
2017-12-04 23:19:04 -0500

    basic code for client

--- client.c
+++ client.c
@@ -1,34 +1,217 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <netdb.h>
-#define DATA "hello word of socket"
+#include <ctype.h>
+#include <dirent.h>
+
+#include "client.h"
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+
+
+
+int portno;
+char* hostname;
+
 int main(int argc, char* argv[]){
+    
+    char* dirName = NULL;
+    char* colName = NULL;
+
+    // check that the args make sense
+    if(argc < 7){
+        printf("Not enough parameters. \n");
+        return 0;
+    }
+    
+    if(argc == 7){
+        colName = argv[3];
+        hostname = argv[5];
+        portno = atoi(argv[7]);
+    }
+
+    if(pthread_mutex_init(&lock, NULL)==-1) printf("mutex failed\n"); 
+    
+
+    if(dirName == NULL){
+        dirName = (char*)malloc(256);
+        dirName = getcwd(dirName, 256);
+
+    }
+
+    struct trav_param* param = (struct trav_param*)malloc(sizeof(struct trav_param));
+    param->rootDirName = dirName;
+    param->colName = colName;
+    traverse(param);
+    free(param);
+
+	return 0;
+}
+
+void* traverse(void* param){
+
+
+	pthread_t* threads = malloc(sizeof(pthread_t)*10);
+    int tct=0;
+	int tsize = 10;
+
+	struct trav_param* trav = (struct trav_param*) param;
+	char* rootDirName = trav->rootDirName; 
+	char* colName = trav->colName;
+
+
+	DIR* root = opendir(rootDirName);
+	if(root == NULL){
+		printf("\n\ninvalid directory\n\n");
+		pthread_exit(NULL);
+	}
+	struct dirent* dp = NULL;
+
+	while((dp = readdir(root)) != NULL) {
+		
+		if((dp->d_name)[0] == '.') continue;
+		char* isdir = isDir(rootDirName, dp->d_name);
+
+		if(isdir){
+
+			
+			pthread_t thread;
+			pthread_mutex_lock(&lock);
+			threadCount++;
+			pthread_mutex_unlock(&lock);
+			struct trav_param* new = (struct trav_param*)malloc(sizeof(struct trav_param));
+			new->rootDirName = isdir;
+			new->colName = colName;
+
+			pthread_create( &thread, NULL, traverse, new);
+			if(tsize == tct){
+				
+				threads = moreThreads(threads, tsize);
+				tsize = tsize*1.5;
+			}
+			threads[tct] = thread;
+			tct++;
+			
+			
+		}
+
+        	FILE* fp = fopen(dp->d_name, "r" );
+		
+            if(!fp){ //get absolute path
+                char* newfile = malloc(strlen(rootDirName) + 1 + strlen(dp->d_name) + 10);
+                strcpy(newfile, rootDirName);
+                strcat(newfile, "/");
+                strcat(newfile, dp->d_name);
+                fp = fopen(newfile, "r");
+                free(newfile);
+            }
+
+
+
+		if(strstr(dp->d_name, ".csv")){  //isCSV
+   
+            if(!strstr(dp->d_name, "-sorted")){
+
+                pthread_t thread;
+                pthread_mutex_lock(&lock);
+                threadCount++;
+                pthread_mutex_unlock(&lock);
+                struct sort_param* new = (struct sort_param*)malloc(sizeof(struct sort_param));
+                new->fp = fp;
+                new->colName = colName;
+
+
+                pthread_create(&thread, NULL, sendFile, new);
+                if(tsize == tct){
+
+                    threads = moreThreads(threads, tsize);
+                    tsize = tsize*1.5;
+                }
+                threads[tct] = thread;
+                tct++;
+
+
+            }
+		}
+				
+	}//end while
+
+	int tm = 0;
+	while(tm < tct){ 
+
+		pthread_join(threads[tm], NULL);
+		tm++;
+
+	}
+
+	if(syscall(__NR_gettid) != initialTID){
+		free(trav);
+		free(threads);
+		pthread_exit(NULL);
+	}
+	free(threads);
+	return NULL;
+}
+
+
+
+void* sendFile(void* param){
+    
+    struct sort_param* sort = (struct sort_param*) param;
+    char* colName = sort->colName;
+    //char* fileName = sort->fileName;
+    FILE* fp = sort->fp;
+    char* titleRow = NULL;
+
+
+
+    // get the first line of the csv
+    rewind(fp);
+
+    titleRow = getRow(fp); 
+    // check if requested column is in the csv
+
+    char* checkTitle = strstr(titleRow, colName);
+    if(checkTitle == NULL){
+        printf("\nRequested column name is not in the csv.\n");
+        fclose(fp);        
+        pthread_exit(NULL);
+
+    }
+
+
+    pthread_mutex_lock(&lock);
+    if(colNum == -2){
+        colNum = getColNum(titleRow, colName);
+    }
+    if(colNum == -1){
+        printf("Error has occured.\n");
+        fclose(fp);        
+        pthread_exit(NULL);
+    }
+    pthread_mutex_unlock(&lock);
+    
+    // create a socket
 	int sock; //the output of method socket()
 	struct sockaddr_in server;// socket struct
 	char buff[1024];//used for the input
 	struct hostent *hp; //used for getting hostname
 
+    sock = socket(AF_INET, SOCK_STREAM,0); //create socket
 
-//	if(argc<7){
-//		printf("error, not even inputs");
-//		exit(1);
-//	}
-
-        sock = socket(AF_INET, SOCK_STREAM,0); //create socket
-	
 	if(sock<0){
 		perror("socket failed");
 		exit(1);
 	}
-	int portno = atoi(argv[6]); //turns input port number to an int
 	server.sin_family = AF_INET; //the address family, AF_INET seems to be standard
-	printf(argv[4]);
-	hp = gethostbyname(argv[4]); //get the hostname from input, and give it to hp using gethostbyname
+	hp = gethostbyname(hostname); //get the hostname from input, and give it to hp using gethostbyname
 	if(hp == 0){
 		perror("get hostbyname failed");
 		exit(1);
@@ -36,20 +219,43 @@ int main(int argc, char* argv[]){
 
 	memcpy(&server.sin_addr, hp->h_addr, hp->h_length); //copies internet address from hp to server socket
 	server.sin_port = htons(9999);
-
+    
+    // connect to socket
 	if(connect(sock, (struct sockaddr *)&server, sizeof(server))<0){
 		perror("connect failed");
 		exit(1);
 	}
-	printf("enter the message: ");
-	fgets(buff, 1023, stdin);
-	
-	if(send(sock, buff,(sizeof(buff)), 0)<0){
-		perror("send failed");
-		exit(1);	
-	}
-	printf("sent message");
-	close(sock);
 
-	return 0;
+    char* row = getRow(fp);
+    // loop through each row and send to socket 
+    while(row!=NULL){
+        row = getRow(fp);
+        // send column number 
+
+        // sending each line to socket goes here
+        /*
+        printf("enter the message: ");
+        fgets(buff, 1023, stdin);
+
+        if(send(sock, buff,(sizeof(buff)), 0)<0){
+            perror("send failed");
+            exit(1);	
+        }
+        */
+
+
+        // send end signal
+
+    }
+    fclose(fp);
+
+    printf("sent message");
+    close(sock);
+
+
+    pthread_exit(NULL);
+    return 0;
 }
+
+
+
---
+++ client.h
@@ -0,0 +1,147 @@
+#ifndef _first_h
+#define _first_h
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <dirent.h>
+
+#include "client.h"
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+int threadCount = 1;
+int initialTID = -1;
+pthread_mutex_t lock;
+int colNum = -2;
+char* title = NULL;
+
+
+struct trav_param{
+	char* rootDirName;
+	char* colName;
+};
+
+struct sort_param{
+	FILE* fp;
+	char* colName;
+
+};
+
+//prototypes
+char* getRow(FILE*);
+int getColNum(char*, char*);
+void* sendFile(void*);
+void* traverse(void*);
+
+// methods
+
+char* isDir(char* rootDirName, char* filename){
+	
+
+	char* path = malloc(strlen(rootDirName) + 1 + strlen(filename) + 20);
+	strcpy(path, rootDirName);
+	strcat(path, "/");
+	strcat(path, filename);
+	
+	int status = 0;
+	struct stat buffer;
+
+	status = stat(path, &buffer);
+
+
+	if(stat == 0){
+		free(path);
+		return NULL;
+
+	}
+
+	if(S_ISREG(buffer.st_mode)){
+		free(path);
+		return NULL;
+	}
+	if(S_ISDIR(buffer.st_mode)){ //return path if directory, else return NULL
+		return path;
+	}
+	free(path);
+	return NULL;
+	
+
+}
+
+pthread_t* moreThreads(pthread_t* lines, int size){
+    int newSize = size * 1.5;
+    pthread_t* temp = NULL;
+
+    temp = (pthread_t*)realloc(lines, newSize * sizeof(pthread_t));
+    if(temp){
+        return temp;    
+    }
+
+    return lines;
+}
+
+char* getRow(FILE* fp){ // add file pointer as param, the alloced row must be freed somewhere
+
+    char* line = calloc(512, sizeof(char));
+    line[0] = 0;
+    char* temp = NULL;
+    int index = 0;
+    char ch = NULL;
+    int size = 512;
+
+    while(ch != '\n' && ch != EOF){
+        ch = getc(fp);  
+        if(index >= size){
+            size = size * 1.5;
+
+            temp = (char*)realloc(line, size);
+            if(temp){
+                line = temp;
+            }
+
+        }
+
+
+        //detect end of file
+        if(feof(fp)){ 
+            return NULL;
+        }
+
+
+        line[index] = ch;
+        index++;
+    }
+
+    line[index+1] = '\0';
+    return line;
+}
+
+int getColNum(char* row1, char* colName){
+    int colCount = 0;
+    char* row = row1;
+    const char* delim = ",\r\n";
+    char* token = strsep(&row,delim);
+    while(token != NULL){
+        if(strcmp(token, colName)==0){
+	    if(title==NULL) title = strdup(row1);
+            return colCount;
+        }
+        
+        token = strsep(&row, delim);
+        colCount++;
+    }
+    return -1;
+}
+
+
+
+
+#endif
