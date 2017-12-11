746f9beafea3be34bee04c92fd2fecb4c3d25806
rl630 <rl630@scarletmail.rutgers.edu>
2017-12-11 12:25:11 -0500

    Update client.c

--- client.c
+++ client.c
@@ -7,12 +7,277 @@
 #include <netdb.h>
 #include <ctype.h>
 #include <dirent.h>
-
+#include <netinet/in.h>
 #include "client.h"
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pthread.h>
+
+//I had to copy and paste the server_thread.h file for some reason when i included it, it didnt pick it up
+
+int threadCount = 1;
+char* outputfile = NULL;
+pthread_mutex_t lock;
+char** globArr = NULL;
+int globArrEnd = 0;
+int numElems = 5000;
+int initialTID = -1;
+int finalSort = 0;
+int type = -1; //0=int, 1=float, 2=string
+char* title = NULL;
+int colNum = -2;
+
+
+struct temp{
+    	int id;
+	int number;
+	char* word;
+	float dec;
+	struct temp* next;
+
+};
+
+typedef struct tNode{
+        pthread_t* curr;
+        struct tNode *next;
+}threadNode;
+
+
+typedef struct temp Node;
+
+struct trav_param{
+	char* rootDirName;
+	char* colName;
+};
+
+struct sort_param{
+	FILE* fp;
+	char* colName;
+
+};
+
+
+//prototypes
+Node* insertAtHead(Node*, Node*);
+char* getRow(FILE*);
+int getColNum(char*, char*);
+char* getCellAtInd(char*, int);
+char* trim(char* str);
+char** moreCapacity(char**, int);
+Node* splitList(Node*);
+Node* mergeInts(Node*, Node*);
+Node* mergeFloats(Node*, Node*);
+Node* floatMergeSort(Node*);
+Node* intMergeSort(Node*);
+Node* mergeStrings(Node*, Node*);
+Node* stringMergeSort(Node*);
+Node* mergeSort(Node*, int);
+void* traverse(void*);
+void* sortFile(void*);
+void insertArr(char*);
+
+
+
+char** moreCapacity(char** lines, int size){
+    int newSize = size * 1.5;
+    char** temp = NULL;
+
+    temp = (char**)realloc(lines, newSize * sizeof(char*));
+    if(temp){
+        return temp;    
+    }
+
+    return NULL;
+}
+
+
+pthread_t* moreThreads(pthread_t* lines, int size){
+    int newSize = size * 1.5;
+    pthread_t* temp = NULL;
+
+    temp = (pthread_t*)realloc(lines, newSize * sizeof(pthread_t));
+    if(temp){
+        return temp;    
+    }
+
+    return lines;
+}
+
+
+
+char* trim(char* str){
+    char* ans;
+
+    // trim leading spaces
+    while(isspace((unsigned char)* str)){
+        str++;
+    }
+    
+    // check if str is all spaces
+    if(*str==0){
+        return str;
+    }
+
+    // trim leading spaces
+    ans = str + strlen(str) -1;
+    while(ans > str && isspace((unsigned char)* ans)){
+        ans--;
+    }
+
+    // null terminate the new string
+    *(ans+1) = 0;
+
+    return str;
+}
+Node* insertAtHead(Node* new1, Node* head){
+	
+	if(head==NULL){
+		new1->next = NULL;
+		return new1;
+	}
+	new1->next = head;
+	return new1;
+
+
+}
+
+
+char* isDir(char* rootDirName, char* filename){
+	
+
+	char* path = malloc(strlen(rootDirName) + 1 + strlen(filename) + 20);
+	strcpy(path, rootDirName);
+	strcat(path, "/");
+	strcat(path, filename);
+	
+	int status = 0;
+	struct stat buffer;
+
+	status = stat(path, &buffer);
+
+
+	if(stat == 0){
+		free(path);
+		return NULL;
+
+	}
+
+	if(S_ISREG(buffer.st_mode)){
+		free(path);
+		return NULL;
+	}
+	if(S_ISDIR(buffer.st_mode)){ //return path if directory, else return NULL
+		return path;
+	}
+	free(path);
+	return NULL;
+	
+
+}
+
+
+
+char* getRow(FILE* fp){ // add file pointer as param, the alloced row must be freed somewhere
+    
+    char* line = calloc(512, sizeof(char));
+    line[0] = 0;
+    char* temp = NULL;
+    int index = 0;
+    char ch = NULL;
+    int size = 512;
+    
+    while(ch != '\n' && ch != EOF){
+        ch = getc(fp);  
+        if(index >= size){
+            size = size * 1.5;
+	   
+            temp = (char*)realloc(line, size);
+	    if(temp){
+            	line = temp;
+	    }
+       
+        }
+	
+
+	//detect end of file
+	if(feof(fp)){ 
+		return NULL;
+	}
+	
+        
+        line[index] = ch;
+        index++;
+    }
+    
+    line[index+1] = '\0';
+    return line;
+}
+
+int getColNum(char* row1, char* colName){
+    int colCount = 0;
+    char* row = row1;
+    const char* delim = ",\r\n";
+    char* token = strsep(&row,delim);
+    while(token != NULL){
+        if(strcmp(token, colName)==0){
+	    if(title==NULL) title = strdup(row1);
+            return colCount;
+        }
+        
+        token = strsep(&row, delim);
+        colCount++;
+    }
+    return -1;
+}
+
+char* getCellAtInd(char* row1, int colNum){ 
+
+    const char* delim = ",\r\n";
+    char* row = row1;
+    char* token = strsep(&row, delim);
+    int index = 0;
+    const char* quoteDelim = "\"";
+    
+
+    while(token != NULL){
+
+        if(token[0] == '\"'){ 	 
+
+            token++;          
+            char* blah = strsep(&row, quoteDelim);
+            char* conc = (char*)calloc(strlen(token) + strlen(blah)+25,1);
+
+            if(index == colNum){
+
+                strcat(conc,token);
+                strcat(conc, ",");
+                strcat(conc,blah);
+                return conc;
+            }
+            free(conc);
+
+            token = strsep(&row, delim);
+        }
+        else{
+
+            if(index == colNum){
+                return token;        
+            }
+
+            token = strsep(&row, delim);
+
+            index++;
+        }
+    }
+
+    return "Error";
+
+}
+
+
+
+
 
 
 
@@ -20,17 +285,99 @@
 int portno;
 char* hostname;
 
+
+void insertArr(char* str){
+
+	if(globArrEnd + 10 > numElems){
+
+		globArr = moreCapacity(globArr, numElems);
+		numElems *= 1.5;
+	}
+
+
+	globArr[globArrEnd] = strdup(str);
+	globArrEnd++;
+
+
+
+
+}
+
+char* recvall(int socket, char *buffer, size_t length, int flags,char *rval){
+	ssize_t n;
+	char *p = buffer;
+	char * remainder;
+	while (strstr(buffer,"srisrisri")==NULL)
+	{
+		n = recv(socket, p, length, flags);
+		if (n == 0)
+			break;
+		else if(n<0)
+			return "ERROR";
+		p += n;
+		length -= n;
+	}
+	buffer[strlen(buffer)]='\0';
+	char *stuff = buffer;
+	char * print;
+	if(strlen(rval)!=0){
+		remainder = malloc(sizeof(char) * (strlen(rval) + strlen(buffer)));
+		strcat(remainder, rval);
+		strcat(remainder,buffer);
+	}else{
+		remainder=malloc(strlen(buffer));
+		strcpy(remainder,buffer);
+
+	}
+	
+	while((stuff=strstr(remainder,"srisrisri"))!=NULL){
+		print = malloc(sizeof(char)*(stuff-remainder) +1);
+		strncpy(print,remainder,(stuff-remainder));
+		print[strlen(print)]='\0';
+
+		
+		insertArr(strdup(print));
+		//free(print); //causes weird printing error
+		remainder = remainder + (stuff-remainder)+9;
+	}
+
+
+	return remainder;
+}
+
+
 int main(int argc, char* argv[]){
-    
+    globArr = (char**)malloc(sizeof(char*) * 5000);
+
     char* dirName = NULL;
     char* colName = NULL;
+	char* outputDir = "./\0"; // This is the directory where the file should be outputted
+	int toOut = 0;
+
+	int i = 0;
+	
+	
 
     // check that the args make sense
     if(argc < 7){
         printf("Not enough parameters. \n");
         return 0;
     }
-    
+	while(i<argc){
+		if(strcmp(argv[i],"-o")==0){
+			outputDir=argv[i+1];
+			toOut=1;
+		}
+		i=i+1;
+	}    
+
+	DIR * tmpb = opendir(outputDir);
+	if(!tmpb){
+		printf("Sorry, that output directory does not exist\n");
+		return 0;
+	}
+	closedir(tmpb);
+
     if(argc == 7){
         colName = argv[2];
         hostname = argv[4];
@@ -51,6 +398,80 @@ int main(int argc, char* argv[]){
     param->colName = colName;
     traverse(param);
     free(param);
+	struct sockaddr_in server;
+	struct hostent *hp; 
+
+	int* sock;
+	sock = socket(AF_INET, SOCK_STREAM,0);
+
+	if(socket<0){
+		perror("Socket Creation failed\n");
+		exit(1);
+	}
+	server.sin_family = AF_INET; 
+	hp = gethostbyname(hostname); 
+	if(hp == 0){
+		perror("Getting hostname failed");
+		exit(1);
+	}
+
+	memcpy(&server.sin_addr, hp->h_addr, hp->h_length); //copies internet address from hp to server socket
+	server.sin_port = htons(9999);
+    
+    // connect to socket
+	if(connect(sock, (struct sockaddr *)&server, sizeof(server))<0){
+		perror("Connection failed");
+		exit(1);
+	}
+	char * finalName = malloc(sizeof(outputDir) + sizeof(colName) + 24);
+	if(toOut==1){
+		if(outputDir[strlen(outputDir)-1]!='/')
+			strcat(outputDir,"/"); 	
+		strcpy(finalName, outputDir);
+	}
+	strcat(finalName, "AllFiles-sorted-");
+	strcat(finalName, colName);
+	strcat(finalName,".csv");
+
+	FILE* h = fopen(finalName, "w+");
+
+	char * rval = "";
+	char buff[10001];
+	char leftover[100001];
+	memset(leftover,0,sizeof(leftover));
+
+
+while(1){
+
+
+		memset(buff,0,sizeof(buff));
+		if(strcmp((rval = recvall(*sock,buff,10000,0,leftover)),"ERROR")==0)
+			perror("reading stream message error");
+
+		else{
+			if(strstr(buff,"borisonufriyev")!=NULL){
+				break;
+			}
+				
+				strcpy(leftover,rval);
+				//row = buff;
+				//if(row==NULL) break;
+				fprintf(h, "%s\n", rval);
+
+		}   
+	}
+
+
+
+
+
+
+	fclose(h);
+
+	close(sock);
+
+
+	
 
 	return 0;
 }
@@ -278,7 +699,3 @@ int sendall(int socket, const void *buffer, size_t length, int flags){
 	//send(socket,"srisrisri", strlen("srisrisri"),0);
     return 0;
 }
-
-
-
-
